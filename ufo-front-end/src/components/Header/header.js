88ec2a62711e7f883417cf08743de4c566cdfc92
Jwilss3 <jessicalaurenwilson1@gmail.com>
2021-07-07 14:26:50 +0100

    updated CSS and added Body component

---
+++ ufo-front-end/src/components/Header/header.js
@@ -0,0 +1,19 @@
+const Header = () => {
+    return (
+        <div>
+    <div>
+     <h1 className={"header"}>Alien Atlas</h1>
+                {/* <img className={"menu_logo"} src={Logo} alt='website logo' /> */}
+        </div>
+        <div className={"navigation"}>
+        <ul>
+            <li>Home</li>
+            <li>Events</li>
+            <li>Locations</li>
+            </ul>
+        </div>
+        </div>
+    )
+}
+
+export default Header
