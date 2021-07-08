a1a264dca4c1afdac841a03b474110346aec3c07
Your Name <lbookatz@hotmail.com>
2021-07-08 12:16:19 +0100

    Andy login stuff

--- ufo-front-end/src/App.js
+++ ufo-front-end/src/App.js
@@ -1,18 +1,29 @@
-import './App.css';
-import Header from './components/Header/header';
-import Body from './components/Body/body';
-import Events from './Pages/Events/';
-import Locations from './Pages/Locations/';
-import img from './img/whiteLogoAndTagline.png';
+import "./App.css";
+import Header from "./components/Header/header";
+import Body from "./components/Body/body";
+import Events from "./Pages/Events/";
+import Locations from "./Pages/Locations/";
+import img from "./img/whiteLogoAndTagline.png";
 
 function App() {
+
+  const [user, setUser] = useState();
+
+  UseEffect(()=>{
+    setUser(getuser());
+  }),[user])
+
   return (
     <div>
-    <img className={"logo"} src={img} alt='website logo' />
+      <div>{user ? <home /> : <logIN />}</div>
+
+      <div>
+        <img className={"logo"} src={img} alt="website logo" />
         <Header />
         <Body />
         <Events />
         <Locations />
+      </div>
     </div>
   );
 }
