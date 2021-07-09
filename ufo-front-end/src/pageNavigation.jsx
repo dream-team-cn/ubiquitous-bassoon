94dbc619af273b0cd7ab7ab8db88480619ddd16f
Becca Mason <beccamason28@gmail.com>
2021-07-09 12:24:54 +0100

    buttons for all 4 pages created and navigation complete

--- ufo-front-end/src/pageNavigation.jsx
+++ ufo-front-end/src/pageNavigation.jsx
@@ -4,6 +4,8 @@ import "./App.css";
 const Navigation = () => {
     const pageEvents = "events";
     const pageLocations = "locations"; 
+    const pageHome = "home";
+    const pageLog = "log activity"
     const [page, setPage] = useState([pageEvents])
     const renderEvents = () => (
         <div>
@@ -15,7 +17,16 @@ const Navigation = () => {
             <h1>Here are the locations (or probs not because it's broke, ha)</h1>
         </div>
     )
-    
+    const renderLog = () => (
+        <div> 
+            <h1>Tell us about your spooky event!</h1>
+        </div>
+    )
+    const renderHome = () => (
+        <div>
+            <h1>Welcome to Alien Atlas! Want to know if extra terrestrials have visited your home town? We can help!</h1>
+        </div>
+    )
     const navigateTo = (nextPage) => {
         setPage(nextPage);
     }
@@ -23,11 +34,15 @@ const Navigation = () => {
     return (
         <div className="navigation">
             <header>
-                <button onClick={() => navigateTo(pageEvents)}>Events</button>
-                <button onClick={() => navigateTo(pageLocations)}>Locations</button>
+                <button className="button1" onClick={() => navigateTo(pageHome)}>Home</button>
+                <button className="button2" onClick={() => navigateTo(pageEvents)}>Events</button>
+                <button className="button3" onClick={() => navigateTo(pageLocations)}>Locations</button>
+                <button className="button4" onClick={() => navigateTo(pageLog)}>Log Activity</button>
             </header>
+            {page === pageHome && renderHome()}
             {page === pageEvents && renderEvents()}
             {page === pageLocations && renderLocations()}
+            {page === pageLog && renderLog()}
         </div>
     )
 }
