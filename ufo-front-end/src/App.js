bded1b2752fe0aacfbf76b569107f9ece8544380
Your Name <lbookatz@hotmail.com>
2021-07-09 09:48:31 +0100

    for Andy

--- ufo-front-end/src/App.js
+++ ufo-front-end/src/App.js
@@ -4,20 +4,50 @@ import Body from "./components/Body/body";
 import Events from "./Pages/Events/";
 import Locations from "./Pages/Locations/";
 import img from "./img/whiteLogoAndTagline.png";
-import {getUser} from "./Utils/user"
-import {useState,useEffect} from "react"
+import { getUser } from "./Utils/user";
+import { useState, useEffect } from "react";
+// import cors from "cors"
+// import express from "express"
+// import app from "express"
+
 
 function App() {
-
+  // console.log("twicw")
   const [user, setUser] = useState();
 
-  useEffect(()=>{
-    setUser(getUser());
-  },[user])
+  // useEffect(() => {
+  //   setUser(getUser());
+  // }, [user]);
+  
+  const [eventjson, seteventjson] = useState({});
+  
+  const getEvents = async () => {
+    console.log("test before const");
+
+      const hope = () => {seteventjson(fetch("http://localhost:5000/events"))}
+    // const hope = await fetch("http://localhost:5000/events");
+    console.log("test after const");
+    console.log(hope);
+  //     method: "GET"
+  //   });
+  //     console.log("why")
+  //   //     "https://api.thecatapi.com/v1/images/search?limit=" + numberOfCats
+  //   //   );
+  //   //   let data = await response.json();
+  //   //   for (let i = 0; i < data.length; i++) {
+  //   //     const cat = data[i];
+  //   //     cat["name"] = faker.name.firstName();
+  //   //     cat["age"] = faker.datatype.number(12);
+  //   //     cat["price"] = faker.commerce.price(50, 100);
+  //   //   }
+  //   //   setCats(data);
+  };
+
+  getEvents();
 
   return (
     <div>
-      <div>{user ? <home /> : <logIN />}</div>
+      {/* <div>{user ? <home /> : <login />}</div> */}
 
       <div>
         <img className={"logo"} src={img} alt="website logo" />
---
+++ ufo-front-end/src/components/Register/register.js
@@ -0,0 +1,26 @@
+// import e = require("express");
+
+// function RegisterUser() {
+
+
+//     handleChange(e) {
+//         this.setState({value: e.target.value});
+//       }
+    
+//       handleSubmit(e) {
+//         alert('A name was submitted: ' + this.state.value);
+//         e.preventDefault();
+
+//   return (
+//     <div>
+//       {/*change the number of cats to display */}
+//       <form>
+//       <input type="text" value={numberOfCats} onChange={onChange} />
+//       <input type="text" value={numberOfCats} onChange={onChange} />
+//       <input type="text" value={numberOfCats} onChange={onChange} />
+
+//       <button onClick={addNumberOfCats}>Number Cats</button>
+//       </form>
+//     </div>
+//   );
+// }
