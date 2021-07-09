16bdec431315dbaf8ce4f34586dc5ee5fcbe558a
Jwilss3 <jessicalaurenwilson1@gmail.com>
2021-07-09 15:00:25 +0100

    added event fetch component

--- ufo-front-end/src/Pages/Events/index.js
+++ ufo-front-end/src/Pages/Events/index.js
@@ -1,9 +1,26 @@
+import { useState, useEffect } from "react";
+
 const Events = () => {
+    const [eventjson, seteventjson] = useState([]);
+
+    const getEvents = async () => {    
+        const hope = await fetch("http://localhost:5000/events/");
+        const data = await hope.json()
+        seteventjson(data);
+};
     return (
         <div>
-            
+            {eventjson.map(event => 
+            <div key={event.id}>{event.date}</div>)}
         </div>
     )
-}
+  };
 
 export default Events
+
+// [{"id":1,
+//         "date":"2009-01-01",
+//         "object":"bright white flashing lights",
+//         "locationid":1,
+//         "createdAt":"2021-07-07T20:34:06.000Z",
+//         "updatedAt":"2021-07-07T20:34:06.000Z"}]
